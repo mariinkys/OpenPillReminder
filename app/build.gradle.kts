9e8ce8c17e19a6922cd94e418ce5a7997f029edb
mariinkys <kysgeneral.nrgua@slmail.me>
2026-05-23 22:15:04 +0200

    chore: up app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "dev.mariinkys.OpenPillReminder"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
